f4234c20bfad9039a6261d765778e7c53c05fc8e
thiru <thiruppathikalimuthu@gamil.com>
2026-01-21 14:00:42 +0530

    Initial commit

---
+++ src/main/kotlin/sap/kotlin/maven/rest/UserController.kt
@@ -0,0 +1,24 @@
+package sap.kotlin.maven.rest
+
+
+import org.springframework.graphql.support.DefaultExecutionGraphQlRequest
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import sap.kotlin.maven.client.GraphQLClient
+import sap.kotlin.maven.model.UserDto
+import java.util.Locale
+
+@RestController
+@RequestMapping("/api")
+class UserController(private val graphQLClient: GraphQLClient) {
+
+    @GetMapping("/users")
+    suspend fun getUsers(): List<UserDto> {
+        return graphQLClient.fetchUsers()
+    }
+}
---
+++ src/test/kotlin/sap/kotlin/maven/SapKotlinMavenApplicationTests.kt
@@ -0,0 +1,13 @@
+package sap.kotlin.maven
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SapKotlinMavenApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
