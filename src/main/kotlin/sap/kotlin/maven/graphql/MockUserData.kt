2e14153a6d57c17e742a293f245f1909a1f6c62e
pravallika.raparthi <raparthipravallika@gmail.com>
2026-01-22 12:28:17 +0530

    my first commit for Kotlin+SpringBoot+Maven project

--- src/main/kotlin/sap/kotlin/maven/graphql/MockUserData.kt
+++ src/main/kotlin/sap/kotlin/maven/graphql/MockUserData.kt
@@ -5,6 +5,6 @@ import sap.kotlin.maven.model.UserDto
 
 val mockUsers = listOf(
     UserDto("1", "Thiru", "thiru@test.com"),
-    UserDto("2", "Kumar", "kumar@test.com"),
+    UserDto("2", "Pravallika", "pravs@test.com"),
     UserDto("3", "Arun", "arun@test.com")
 )
--- src/main/kotlin/sap/kotlin/maven/graphql/UserGraphQLController.kt
+++ src/main/kotlin/sap/kotlin/maven/graphql/UserGraphQLController.kt
@@ -1,6 +1,7 @@
 package sap.kotlin.maven.graphql
 
 
+import org.springframework.graphql.data.method.annotation.Argument
 import sap.kotlin.maven.model.UserDto
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
@@ -11,5 +12,11 @@ class UserGraphQLController {
     @QueryMapping
     fun users(): List<UserDto> {
         return mockUsers
+    }
+
+    //to get single user when given the user id
+    @QueryMapping
+    fun user(@Argument id: String): UserDto? {
+        return mockUsers.find { it.id == id }
     }
 }
