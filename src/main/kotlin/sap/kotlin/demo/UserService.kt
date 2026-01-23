3c6667acac903e81802b25d1b4b731862ab6490c
Subashini <thusara.00@gmail.com>
2026-01-23 18:31:38 +0530

    DynamoDb Changes

--- src/main/kotlin/sap/kotlin/demo/UserService.kt
+++ src/main/kotlin/sap/kotlin/demo/UserService.kt
@@ -1,21 +1,32 @@
 package sap.kotlin.demo
 
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.CachePut
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
-import java.util.UUID
+import java.util.*
 
 @Service
 class UserService(private val repo: UserRepository) {
 
+    @CachePut(value = ["users"], key = "#result.id")
     fun save(name: String, email: String): User =
         repo.save(
             User().apply {
+                println("Inside save of dynamodb")
                 id = UUID.randomUUID().toString()
                 this.name = name
                 this.email = email
+                println("Done save using dynamodb")
             }
         )
 
-    fun findById(id: String) = repo.findById(id)
+    @Cacheable(value = ["users"], key = "#id")
+    fun findById(id: String): User? {
+        return repo.findById(id)
+    }
+
     fun findAll() = repo.findAll()
+    @CacheEvict(value = ["users"], key = "#id")
     fun deleteById(id: String) = repo.deleteById(id)
 }
